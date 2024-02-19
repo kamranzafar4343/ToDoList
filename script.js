f2755ef4c677fe7f00bd1787fc4f54b1ccbd8781
Kamran Zafar <kamranzafar4343@gmail.com>
2024-02-19 23:38:56 +0500

    fix - javascript was not added to html

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
-    
+
+    if(inputBox.value === ''){
+        alert("Please add some task");
+    }
+
+    else{
+        const li = document.getElementById("li"); //stores li
+        li.innerHTML = inputBox.value; //stores value entered by user in input box
+        listContainer.appendChild(li); //displays the li
+    }
 }
